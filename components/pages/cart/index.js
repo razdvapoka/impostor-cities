5eaf681f984298258405d4877b88e3dc2c638bec
razdvapoka <razdvapoka@gmail.com>
2021-05-17 00:53:22 +0300

    :sparkles: Add checkout

--- components/pages/cart/index.js
+++ components/pages/cart/index.js
@@ -1,9 +1,13 @@
 // import useTranslation from 'next-translate/useTranslation'
+import { useCallback, useMemo } from 'react'
 import { Layout } from '@/components'
 import { useCart } from '@/contexts/cart'
 import styles from './styles.module.scss'
 import { getPriceString, getSizeOption, getColorOption } from '@/lib/utils'
 import cn from 'classnames'
+import DeleteIcon from '../../../assets/icons/delete.svg'
+import IncIcon from '../../../assets/icons/inc.svg'
+import DecIcon from '../../../assets/icons/dec.svg'
 
 const getVariants = (product, variantId) => {
   const variantEn = product.en.variants.find((v) => v.id === variantId)
@@ -11,79 +15,187 @@ const getVariants = (product, variantId) => {
   return { variantEn, variantFr }
 }
 
-const CartProduct = ({ product, variant: { variantId, count } }) => {
+const CartProduct = ({
+  product,
+  variant: { variantId, count },
+  cart,
+  setCart,
+}) => {
   const { variantEn, variantFr } = getVariants(product, variantId)
+  const deleteVariant = useCallback(() => {
+    const productId = product.en.id
+    setCart({
+      ...cart,
+      [productId]: cart[productId].filter((v) => v.variantId !== variantId),
+    })
+  }, [product, variantId, cart, setCart])
+  const decreaseNumber = useCallback(() => {
+    const productId = product.en.id
+    setCart({
+      ...cart,
+      [productId]: cart[productId].map((v) =>
+        v.variantId === variantId && v.count > 1
+          ? { ...v, count: v.count - 1 }
+          : v
+      ),
+    })
+  }, [product, variantId, cart, setCart])
+  const increaseNumber = useCallback(() => {
+    const productId = product.en.id
+    setCart({
+      ...cart,
+      [productId]: cart[productId].map((v) =>
+        v.variantId === variantId ? { ...v, count: v.count + 1 } : v
+      ),
+    })
+  }, [product, variantId, cart, setCart])
+  const colorEn = getColorOption(variantEn)?.value
+  const colorFr = getColorOption(variantFr)?.value
+  const sizeEn = getSizeOption(variantEn)?.value
+  const sizeFr = getSizeOption(variantFr)?.value
   return (
     <div className="my-grid text-ts1B">
       <div className="w-1/8">{product.en.title}</div>
       <div className="w-1/8">{product.fr.title}</div>
-      <div className="w-1/16">{getColorOption(variantEn).value}</div>
-      <div className="w-1/8">{getColorOption(variantFr).value}</div>
-      <div className="w-1/16">{getSizeOption(variantEn).value}</div>
-      <div className="w-1/8">{getSizeOption(variantFr).value}</div>
-      <div className="w-1/8">{count}</div>
+      <div className="w-1/16">{colorEn || '—'}</div>
+      <div className="w-1/8">{colorFr || '—'}</div>
+      <div className="w-1/16">{sizeEn || '—'}</div>
+      <div className="w-1/8">{sizeFr || '—'}</div>
+      <div className="w-1/8">
+        <div className="flex">
+          <div>{count}</div>
+          <button
+            className={cn(styles.decButton, 'text-grey ml-3')}
+            onClick={decreaseNumber}
+          >
+            <DecIcon />
+          </button>
+          <button
+            className={cn(styles.incButton, 'text-grey')}
+            onClick={increaseNumber}
+          >
+            <IncIcon />
+          </button>
+        </div>
+      </div>
       <div className="w-1/8">{getPriceString(variantEn.price)}</div>
+      <div className="w-1/8">
+        <button
+          className={cn(
+            styles.deleteButton,
+            // 'hover:text-grey transition-colors'
+            'text-grey'
+          )}
+          onClick={deleteVariant}
+        >
+          <DeleteIcon />
+        </button>
+      </div>
+    </div>
+  )
+}
+
+const CartHeader = () => {
+  return (
+    <div className="pt-3 mb-1 my-grid text-ts1B">
+      <div className="w-1/8">
+        <div className="pb-8">Item</div>
+      </div>
+      <div className="w-1/8">
+        <div className={cn('pb-8 relative', styles.leftBorder)}>Article</div>
+      </div>
+      <div className="w-1/16">
+        <div className={cn('pb-8 relative', styles.leftBorder)}>Colour</div>
+      </div>
+      <div className="w-1/8">
+        <div className={cn('pb-8 relative', styles.leftBorder)}>Couleur</div>
+      </div>
+      <div className="w-1/16">
+        <div className={cn('pb-8 relative', styles.leftBorder)}>Size</div>
+      </div>
+      <div className="w-1/8">
+        <div className={cn('pb-8 relative', styles.leftBorder)}>Grandeur</div>
+      </div>
+      <div className="w-1/16">
+        <div className={cn('pb-8 relative', styles.leftBorder)}>Qty</div>
+      </div>
+      <div className="w-1/16">
+        <div className="relative pb-8">Qté</div>
+      </div>
+      <div className="w-1/16">
+        <div className={cn('pb-8 relative', styles.leftBorder)}>Price</div>
+      </div>
+      <div className="w-1/16">
+        <div className="relative pb-8">Prix</div>
+      </div>
     </div>
   )
 }
 
 const Cart = ({ commonData, products }) => {
-  const [
-    cart,
-    // setCart
-  ] = useCart()
-  const productMap = products.reduce(
-    (map, product) => ({
-      ...map,
-      [product.en.id]: product,
-    }),
-    {}
+  const [cart, setCart] = useCart()
+
+  const productMap = useMemo(
+    () =>
+      products.reduce(
+        (map, product) => ({
+          ...map,
+          [product.en.id]: product,
+        }),
+        {}
+      ),
+    [products]
   )
+
+  const cartTotal = useMemo(
+    () =>
+      Object.keys(cart).reduce((total, productKey) => {
+        const product = productMap[productKey]
+        const variants = cart[productKey]
+        return (
+          total +
+          variants.reduce(
+            (productTotal, { count, variantId }) =>
+              productTotal +
+              count *
+                parseInt(
+                  product.en.variants.find((v) => v.id === variantId).price
+                ),
+            0
+          )
+        )
+      }, 0),
+    [products, cart]
+  )
+
+  const handleCheckout = useCallback(() => {
+    const lineItems = Object.values(cart).flatMap((variants) =>
+      variants.map(({ variantId, count }) => ({
+        variantId,
+        quantity: count,
+      }))
+    )
+    fetch('/api/checkout', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ lineItems }),
+    })
+      .then((response) => response.json())
+      .then(({ url }) => {
+        window.open(url, '_blank')
+      })
+      .catch((err) => {
+        console.log('ERROR', err)
+      })
+  }, [cart])
+
   return (
     <Layout {...commonData}>
       <div className={cn('', styles.cart)}>
-        <div className="my-grid pt-3 text-ts1B mb-1">
-          <div className="w-1/8">
-            <div className="pb-8">Item</div>
-          </div>
-          <div className="w-1/8">
-            <div className={cn('pb-8 relative', styles.leftBorder)}>
-              Article
-            </div>
-          </div>
-          <div className="w-1/16">
-            <div className={cn('pb-8 relative', styles.leftBorder)}>Colour</div>
-          </div>
-          <div className="w-1/8">
-            <div className={cn('pb-8 relative', styles.leftBorder)}>
-              Couleur
-            </div>
-          </div>
-          <div className="w-1/16">
-            <div className={cn('pb-8 relative', styles.leftBorder)}>Size</div>
-          </div>
-          <div className="w-1/8">
-            <div className={cn('pb-8 relative', styles.leftBorder)}>
-              Grandeur
-            </div>
-          </div>
-          <div className="w-1/16">
-            <div className={cn('pb-8 relative', styles.leftBorder)}>Qty</div>
-          </div>
-          <div className="w-1/16">
-            <div className="relative pb-8">Qté</div>
-          </div>
-          <div className="w-1/16">
-            <div className={cn('pb-8 relative', styles.leftBorder)}>Price</div>
-          </div>
-          <div className="w-1/16">
-            <div className="relative pb-8">Prix</div>
-          </div>
-          <div className="w-1/8">
-            <div className="relative pb-8">Buttons</div>
-          </div>
-        </div>
-        <div>
+        <CartHeader />
+        <div className="border-b-2 border-white border-solid pb-15">
           {Object.keys(cart).map((key) => {
             const product = productMap[key]
             const selectedVariants = cart[key]
@@ -94,11 +206,44 @@ const Cart = ({ commonData, products }) => {
                     key={variant.variantId}
                     product={product}
                     variant={variant}
+                    cart={cart}
+                    setCart={setCart}
                   />
                 ))}
               </div>
             )
           })}
+        </div>
+        <div className="pt-2 my-grid">
+          <div className="w-6/8" />
+          <div className="w-1/16 text-ts1B">Total</div>
+          <div className="w-1/16 text-ts1B">Totale</div>
+        </div>
+        <div className="pt-2 pb-20 border-b-2 border-white border-solid my-grid">
+          <div className="w-6/8" />
+          <div className="w-2/8 text-ts2">{getPriceString(cartTotal)}</div>
+        </div>
+        <div className="pt-2 my-grid">
+          <div className="w-4/8" />
+          <div className="w-2/8">
+            <div className="text-ts1B">
+              Shipping & taxes <br />
+              calculated at checkout
+            </div>
+            <button onClick={handleCheckout} className="mt-8 text-ts2">
+              Checkout
+            </button>
+          </div>
+          <div className="w-2/8">
+            <div className="text-ts1B">
+              Expédition et taxes
+              <br />
+              calculé à la caisse
+            </div>
+            <button onClick={handleCheckout} className="mt-8 text-ts2">
+              Passer à la caisse
+            </button>
+          </div>
         </div>
       </div>
     </Layout>
---
+++ pages/api/checkout.js
@@ -0,0 +1,14 @@
+import Client from 'shopify-buy'
+
+const client = Client.buildClient({
+  domain: process.env.SHOPIFY_DOMAIN,
+  storefrontAccessToken: process.env.SHOPIFY_ACCESS_TOKEN,
+})
+
+const handler = async ({ body: { lineItems } }, res) => {
+  const checkout = await client.checkout.create()
+  await client.checkout.addLineItems(checkout.id, lineItems)
+  res.status(200).json({ url: checkout.webUrl })
+}
+
+export default handler
