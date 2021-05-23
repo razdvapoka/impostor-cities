9baa2f41c0da8b874bbae41afcca1dc1cf2fc9a3
razdvapoka <razdvapoka@gmail.com>
2021-05-23 23:37:58 +0300

    :construction: Adapt cart page

--- components/pages/cart/index.js
+++ components/pages/cart/index.js
@@ -1,9 +1,15 @@
-// import useTranslation from 'next-translate/useTranslation'
+import useTranslation from 'next-translate/useTranslation'
 import { useCallback, useMemo } from 'react'
-import { Dummy, Layout } from '@/components'
+import { Layout } from '@/components'
 import { useCart } from '@/contexts/cart'
 import styles from './styles.module.scss'
-import { getPriceString, getSizeOption, getColorOption } from '@/lib/utils'
+import {
+  getPriceString,
+  getSizeOption,
+  getColorOption,
+  enOnly,
+  frOnly,
+} from '@/lib/utils'
 import cn from 'classnames'
 import DeleteIcon from '../../../assets/icons/delete.svg'
 import IncIcon from '../../../assets/icons/inc.svg'
@@ -95,161 +101,212 @@ const CartProduct = ({
   )
 }
 
-const CartHeader = () => {
+const TotalHeader = () => {
+  const { lang } = useTranslation('common')
   return (
-    <div className="pt-3 mb-1 my-grid text-ts1B">
-      <div className="w-1/8">
+    <div className="pt-2 my-grid mobile:text-grey">
+      <div className="w-6/8 mobile:w-4/8" />
+      <div className={cn('w-1/16 text-ts1B mobile:w-4/8', enOnly(lang))}>
+        Total
+      </div>
+      <div className={cn('w-1/16 text-ts1B mobile:w-4/8', frOnly(lang))}>
+        Totale
+      </div>
+    </div>
+  )
+}
+const Total = ({ total }) => {
+  return (
+    <div className="pt-2 pb-20 border-b-2 border-white mobile:pt-5 mobile:pb-10 mobile:border-b-0 my-grid">
+      <div className="w-6/8 mobile:w-4/8" />
+      <div className="w-2/8 text-ts2 mobile:text-ts3B mobile:w-4/8">
+        {getPriceString(total)}
+      </div>
+    </div>
+  )
+}
+
+const Checkout = ({ handleCheckout }) => {
+  return (
+    <div className="pt-2 my-grid">
+      <div className="w-4/8" />
+      <div className="w-2/8">
+        <div className="text-ts1B">
+          Shipping & taxes
+          <br />
+          calculated at checkout
+        </div>
+        <button onClick={handleCheckout} className="mt-8 text-ts2">
+          Checkout
+        </button>
+      </div>
+      <div className="w-2/8">
+        <div className="text-ts1B">
+          Expédition et taxes
+          <br />
+          calculé à la caisse
+        </div>
+        <button onClick={handleCheckout} className="mt-8 text-ts2">
+          Passer à la caisse
+        </button>
+      </div>
+    </div>
+  )
+}
+
+const CartHeader = () => {
+  const { lang } = useTranslation('common')
+  return (
+    <div className="pt-3 mb-1 my-grid text-ts1B mobile:pt-2">
+      <div className={cn('w-1/8 mobile:w-4/8 mobile:text-grey', enOnly(lang))}>
         <div className="pb-8">Item</div>
       </div>
-      <div className="w-1/8">
+      <div className={cn('w-1/8 mobile:w-4/8 mobile:text-grey', frOnly(lang))}>
         <div className={cn('pb-8 relative', styles.leftBorder)}>Article</div>
       </div>
-      <div className="w-1/16">
+      <div className="w-1/16 mobile:hidden">
         <div className={cn('pb-8 relative', styles.leftBorder)}>Colour</div>
       </div>
-      <div className="w-1/8">
+      <div className="w-1/8 mobile:hidden">
         <div className={cn('pb-8 relative', styles.leftBorder)}>Couleur</div>
       </div>
-      <div className="w-1/16">
+      <div className="w-1/16 mobile:hidden">
         <div className={cn('pb-8 relative', styles.leftBorder)}>Size</div>
       </div>
-      <div className="w-1/8">
+      <div className="w-1/8 mobile:hidden">
         <div className={cn('pb-8 relative', styles.leftBorder)}>Grandeur</div>
       </div>
-      <div className="w-1/16">
+      <div
+        className={cn(
+          'w-1/16 mobile:order-2 mobile:w-2/8 mobile:text-grey',
+          enOnly(lang)
+        )}
+      >
         <div className={cn('pb-8 relative', styles.leftBorder)}>Qty</div>
       </div>
-      <div className="w-1/16">
+      <div
+        className={cn(
+          'w-1/16 mobile:order-2 mobile:w-2/8 mobile:text-grey',
+          frOnly(lang)
+        )}
+      >
         <div className="relative pb-8">Qté</div>
       </div>
-      <div className="w-1/16">
+      <div
+        className={cn(
+          'w-1/16 mobile:order-1 mobile:w-2/8 mobile:text-grey',
+          enOnly(lang)
+        )}
+      >
         <div className={cn('pb-8 relative', styles.leftBorder)}>Price</div>
       </div>
-      <div className="w-1/16">
+      <div
+        className={cn(
+          'w-1/16 mobile:order-1 mobile:w-2/8 mobile:text-grey',
+          frOnly(lang)
+        )}
+      >
         <div className="relative pb-8">Prix</div>
       </div>
     </div>
   )
 }
 
+const Products = ({ cart, setCart, productMap }) => {
+  return (
+    <div className="border-b-2 border-white mobile:border-b-1 pb-15">
+      {Object.keys(cart).map((key) => {
+        const product = productMap[key]
+        const selectedVariants = cart[key]
+        return (
+          <div key={key}>
+            {selectedVariants.map((variant) => (
+              <CartProduct
+                key={variant.variantId}
+                product={product}
+                variant={variant}
+                cart={cart}
+                setCart={setCart}
+              />
+            ))}
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+const getProductMap = (products) => () =>
+  products.reduce(
+    (map, product) => ({
+      ...map,
+      [product.en.id]: product,
+    }),
+    {}
+  )
+
+const getCartTotal = (cart, productMap) =>
+  Object.keys(cart).reduce((total, productKey) => {
+    const product = productMap[productKey]
+    const variants = cart[productKey]
+    return (
+      total +
+      variants.reduce(
+        (productTotal, { count, variantId }) =>
+          productTotal +
+          count *
+            parseInt(product.en.variants.find((v) => v.id === variantId).price),
+        0
+      )
+    )
+  }, 0)
+
+const getLineItems = (cart) =>
+  Object.values(cart).flatMap((variants) =>
+    variants.map(({ variantId, count }) => ({
+      variantId,
+      quantity: count,
+    }))
+  )
+
+const checkout = (lineItems) =>
+  fetch('/api/checkout', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ lineItems }),
+  })
+    .then((response) => response.json())
+    .then(({ url }) => {
+      window.open(url, '_blank')
+    })
+    .catch((err) => {
+      console.log('ERROR', err)
+    })
+
 const Cart = ({ commonData, products }) => {
   const [cart, setCart] = useCart()
 
-  const productMap = useMemo(
-    () =>
-      products.reduce(
-        (map, product) => ({
-          ...map,
-          [product.en.id]: product,
-        }),
-        {}
-      ),
-    [products]
-  )
+  const productMap = useMemo(() => getProductMap(products), [products])
 
-  const cartTotal = useMemo(
-    () =>
-      Object.keys(cart).reduce((total, productKey) => {
-        const product = productMap[productKey]
-        const variants = cart[productKey]
-        return (
-          total +
-          variants.reduce(
-            (productTotal, { count, variantId }) =>
-              productTotal +
-              count *
-                parseInt(
-                  product.en.variants.find((v) => v.id === variantId).price
-                ),
-            0
-          )
-        )
-      }, 0),
-    [products, cart]
-  )
+  const cartTotal = useMemo(() => getCartTotal(cart, productMap), [
+    cart,
+    productMap,
+  ])
 
   const handleCheckout = useCallback(() => {
-    const lineItems = Object.values(cart).flatMap((variants) =>
-      variants.map(({ variantId, count }) => ({
-        variantId,
-        quantity: count,
-      }))
-    )
-    fetch('/api/checkout', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ lineItems }),
-    })
-      .then((response) => response.json())
-      .then(({ url }) => {
-        window.open(url, '_blank')
-      })
-      .catch((err) => {
-        console.log('ERROR', err)
-      })
+    checkout(getLineItems(cart))
   }, [cart])
 
   return (
     <Layout {...commonData}>
-      <div className="hidden mobile:block">
-        <Dummy />
-      </div>
-      <div className="mobile:hidden">
-        <div className={cn('', styles.cart)}>
-          <CartHeader />
-          <div className="border-b-2 border-white border-solid pb-15">
-            {Object.keys(cart).map((key) => {
-              const product = productMap[key]
-              const selectedVariants = cart[key]
-              return (
-                <div key={key}>
-                  {selectedVariants.map((variant) => (
-                    <CartProduct
-                      key={variant.variantId}
-                      product={product}
-                      variant={variant}
-                      cart={cart}
-                      setCart={setCart}
-                    />
-                  ))}
-                </div>
-              )
-            })}
-          </div>
-          <div className="pt-2 my-grid">
-            <div className="w-6/8" />
-            <div className="w-1/16 text-ts1B">Total</div>
-            <div className="w-1/16 text-ts1B">Totale</div>
-          </div>
-          <div className="pt-2 pb-20 border-b-2 border-white border-solid my-grid">
-            <div className="w-6/8" />
-            <div className="w-2/8 text-ts2">{getPriceString(cartTotal)}</div>
-          </div>
-          <div className="pt-2 my-grid">
-            <div className="w-4/8" />
-            <div className="w-2/8">
-              <div className="text-ts1B">
-                Shipping & taxes <br />
-                calculated at checkout
-              </div>
-              <button onClick={handleCheckout} className="mt-8 text-ts2">
-                Checkout
-              </button>
-            </div>
-            <div className="w-2/8">
-              <div className="text-ts1B">
-                Expédition et taxes
-                <br />
-                calculé à la caisse
-              </div>
-              <button onClick={handleCheckout} className="mt-8 text-ts2">
-                Passer à la caisse
-              </button>
-            </div>
-          </div>
-        </div>
+      <div className={cn('mobile:border-t-1 mobile:border-white', styles.cart)}>
+        <CartHeader />
+        <Products cart={cart} setCart={setCart} productMap={productMap} />
+        <TotalHeader />
+        <Total total={cartTotal} />
+        <Checkout handleCheckout={handleCheckout} />
       </div>
     </Layout>
   )
