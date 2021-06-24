1a98125d90bf35df068cacef77a3dd6d50860141
razdvapoka <razdvapoka@gmail.com>
2021-06-24 19:53:50 +0300

    :fire: Disable locales from cms

--- i18n.js
+++ i18n.js
@@ -2,11 +2,11 @@ module.exports = {
   locales: ['en', 'fr'],
   defaultLocale: 'en',
   localeDetection: true,
-  loadLocaleFrom: (locale) => {
-    return fetch(
-      `${process.env.NEXT_PUBLIC_HOST_URL}/api/locales/${locale}`
-    ).then((response) => response.json())
-  },
+  // loadLocaleFrom: (locale) => {
+  //   return fetch(
+  //     `${process.env.NEXT_PUBLIC_HOST_URL}/api/locales/${locale}`
+  //   ).then((response) => response.json())
+  // },
   pages: {
     '*': ['common'],
   },
