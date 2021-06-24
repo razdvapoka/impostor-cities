8c7fafc929cdecafc8dd505d855759cdc33584e8
razdvapoka <razdvapoka@gmail.com>
2021-06-24 22:01:02 +0300

    :sparkles: Load locales from cms

---
+++ get-locales.js
@@ -0,0 +1,13 @@
+const fetch = require('node-fetch')
+const fs = require('fs/promises')
+
+fetch(`https://impostorcities.com/api/locales`)
+  .then((response) => response.json())
+  .then(({ en, fr }) => {
+    return Promise.all([
+      fs.writeFile('./locales/en/common.json', JSON.stringify(en)),
+      fs.writeFile('./locales/fr/common.json', JSON.stringify(fr)),
+    ]).then(() => {
+      console.log('locales got')
+    })
+  })
